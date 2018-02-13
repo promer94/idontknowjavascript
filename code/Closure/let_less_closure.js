f840599f6a6b93f832cda6b5852557666ec53500
yixuanxu94 <yixuanxu94@gmail.com>
2018-02-13 03:14:03 +0000

    closure

---
+++ code/Closure/let_less_closure.js
@@ -0,0 +1,21 @@
+/* 使用 let关键字可以避免产生过多的闭包 */
+function showHelp(help) {
+    document.getElementById('help').innerHTML = help;
+  }
+  
+  function setupHelp() {
+    var helpText = [
+        {'id': 'email', 'help': 'Your e-mail address'},
+        {'id': 'name', 'help': 'Your full name'},
+        {'id': 'age', 'help': 'Your age (you must be over 16)'}
+      ];
+  
+    for (var i = 0; i < helpText.length; i++) {
+      let item = helpText[i];
+      document.getElementById(item.id).onfocus = function() {
+        showHelp(item.help);
+      }
+    }
+  }
+  
+  setupHelp();
---
+++ code/Closure/multiple_closure.js
@@ -0,0 +1,25 @@
+/* 使用函数工厂的模式创建多个闭包 */
+function showHelp(help) {
+    document.getElementById('help').innerHTML = help;
+  }
+  
+  function makeHelpCallback(help) {
+    return function() {
+      showHelp(help);
+    };
+  }
+  
+  function setupHelp() {
+    var helpText = [
+        {'id': 'email', 'help': 'Your e-mail address'},
+        {'id': 'name', 'help': 'Your full name'},
+        {'id': 'age', 'help': 'Your age (you must be over 16)'}
+      ];
+  
+    for (var i = 0; i < helpText.length; i++) {
+      var item = helpText[i];
+      document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
+    }
+  }
+  
+  setupHelp();
