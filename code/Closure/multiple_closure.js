03a4b4c9f68648c5cd89c95c62ce8965817d4f0b
Yixuan Xu <yixuanxu94@gmail.com>
2018-02-14 23:43:17 +0000

    Anonymous function create closure

--- code/Closure/multiple_closure.js
+++ code/Closure/multiple_closure.js
@@ -16,10 +16,32 @@ function showHelp(help) {
         {'id': 'age', 'help': 'Your age (you must be over 16)'}
       ];
   
-    for (var i = 0; i < helpText.length; i++) {
-      var item = helpText[i];
-      document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
+  for (var i = 0; i < helpText.length; i++) {
+    var item = helpText[i];
+    document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
     }
   }
   
   setupHelp();
+
+  /*使用匿名函数创建闭包*/
+
+  function setupHelpAnonymous(){
+    var helpText = [
+      {'id': 'email', 'help': 'Your e-mail address'},
+      {'id': 'name', 'help': 'Your full name'},
+      {'id': 'age', 'help': 'Your age (you must be over 16)'}
+    ];
+
+    for(var i = 0; i < helpText.length; i++){
+      var item = helpText[i];
+      (function() {
+        var item = helpText[i];
+        document.getElementById(item.id).onfocus = function() {
+          showHelp(item.help);
+        }
+     })();
+    }
+  }
+
+  setupHelpAnonymous()
