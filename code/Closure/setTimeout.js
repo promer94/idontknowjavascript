2230827cd4d797f178ffba927b6e76b7bfa25375
Yixuan Xu <yixuanxu94@gmail.com>
2018-07-29 09:08:19 +0100

    :beer: update

--- code/Closure/setTimeout.js
+++ code/Closure/setTimeout.js
@@ -1,17 +1,21 @@
-for(var a = 0; a < 5; a +=1){//eslint-disable-line
-  console.log(`a = ${a}`); //eslint-disable-line
+for (var a = 0; a < 5; a += 1) {
+  //eslint-disable-line
+  console.log(`a = ${a}`) //eslint-disable-line
 }
 
-for(var b = 0; b < 5; b +=1){//eslint-disable-line
-  setTimeout(() => { //eslint-disable-line
-    console.log(`b = ${b}`); //eslint-disable-line
+for (var b = 0; b < 5; b += 1) {
+  //eslint-disable-line
+  setTimeout(() => {
+    //eslint-disable-line
+    console.log(`b = ${b}`) //eslint-disable-line
   }, 1000 * b)
 }
 
-for(var c = 0; c < 5; c +=1){ //eslint-disable-line
+for (var c = 0; c < 5; c += 1) {
+  //eslint-disable-line
   ;(function(c) {
     setTimeout(() => {
-      console.log(`c = ${c}`); //eslint-disable-line
+      console.log(`c = ${c}`) //eslint-disable-line
     }, c * 1000)
   })(c)
 }
@@ -19,8 +23,9 @@ for(var c = 0; c < 5; c +=1){ //eslint-disable-line
 for (var d = 0; d < 5; d += 1) {
   //eslint-disable-line
   setTimeout(
-    (function() {  //eslint-disable-line
-      console.log(`d = ${d}`); //eslint-disable-line
+    (function() {
+      //eslint-disable-line
+      console.log(`d = ${d}`) //eslint-disable-line
     })(),
     d * 1000
   )
@@ -28,6 +33,6 @@ for (var d = 0; d < 5; d += 1) {
 
 for (let e = 0; e < 5; e += 1) {
   setTimeout(() => {
-    console.log(`e = ${e}`); //eslint-disable-line
+    console.log(`e = ${e}`) //eslint-disable-line
   }, 1000 * e)
 }
