2e66b05ee86869a99300b010c201cd16807e1fe9
Yixuan Xu <yixuanxu94@gmail.com>
2018-09-30 20:34:02 +0800

    :fire: xmlhttprequest

--- code/Async/promiseXmlHttpRequest.js
+++ code/Async/promiseXmlHttpRequest.js
@@ -1,10 +1,13 @@
-function request(url) {
+export function request(url) {
   //eslint-disable-line
   const oReq = new XMLHttpRequest()
 
   return new Promise((resolve, reject) => {
     oReq.open('GET', url)
-    oReq.addEventListener('load', data => resolve(data))
+    oReq.addEventListener('load', ({ srcElement: { response, status } }) => {
+      response = JSON.parse(response)
+      resolve({ status, response })
+    })
     oReq.addEventListener('error', error => reject(error))
     oReq.send()
   })
