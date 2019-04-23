609cf73f986064ed797ec0e1ac03f6817ed958ad
Yixuan Xu <yixuanxu94@gmail.com>
2019-04-23 23:04:34 +0800

    promise in order

--- code/Async/promiseFirst.js
+++ code/Async/promiseFirst.js
@@ -20,3 +20,41 @@ if (!Promise.first) {
     })
   }
 }
+
+// without async
+if (!Promise.order) {
+  Promise.order = function(tasks) {
+    if (!Array.isArray(tasks))
+      throw new Error('Promise should be contained in an Array')
+    return tasks.reduce((promiseChain, currentPromise) => {
+      return promiseChain.then(results =>
+        currentPromise
+          .then(result => {
+            return results.concat(result)
+          })
+          .catch(() => {
+            return results.concat(null)
+          })
+      )
+    }, Promise.resolve().then(() => []))
+  }
+}
+
+// with async
+if (!Promise.order) {
+  Promise.order = async function(tasks) {
+    const results = []
+    if (!Array.isArray(tasks))
+      throw new Error('Promise should be contained in an Array')
+    for (const task of tasks) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const data = await task
+        results.push(data)
+      } catch (e) {
+        results.push(null)
+      }
+    }
+    return results
+  }
+}
