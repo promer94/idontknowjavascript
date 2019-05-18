98c2ae8a27e7bfd61aa3c7105c366892ae5bbe2e
Yixuan Xu <yixuanxu94@gmail.com>
2019-05-18 09:26:09 +0800

    improve code

--- code/Async/generatorRunner.js
+++ code/Async/generatorRunner.js
@@ -1,20 +1,17 @@
-// thanks to Benjamin Gruenbaum (@benjamingr on GitHub) for
-// big improvements here!
 function run(...args) {
   // initialize the generator in the current context
-  const it = args[0].apply(args[0], args.slice[1])
+  const [fn, ...data] = args
+  const it = fn.apply(fn, data)
 
   // return a promise for the generator completing
   return Promise.resolve().then(function handleNext(value) {
     // run to the next yielded value
     const next = it.next(value)
-    return (function handleResult(next) {
-      return next.done
-        ? next.value
-        : Promise.resolve(next.value).then(handleNext, err => {
-            return Promise.resolve(it.throw(err)).then(handleResult)
-          })
-    })(next)
+    return next.done
+      ? next.value
+      : Promise.resolve(next.value).then(handleNext, err => {
+          return Promise.resolve(it.throw(err)).then(() => next.value)
+        })
   })
 }
 function doLater(a, b) {
@@ -25,16 +22,17 @@ function doLater(a, b) {
   })
 }
 
-function* main() {
+function* main(c, d) {
   try {
-    const a = doLater(1, 2)
-    const b = doLater(2, 3)
+    const a = doLater(1, c)
+    const b = doLater(2, d)
     const result = yield a
     const num = yield b
     console.log(result + num)
+    return result + num
   } catch (e) {
     console.log(e.message)
   }
 }
 
-run(main)
+run(main, 1, 2).then(v => console.log('v', v))
